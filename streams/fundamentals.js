d9eca2715406daff7ccb9316e7da69d437d30805
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-18 11:43:04 -0300

    Criando stream de leitura

---
+++ streams/fundamentals.js
@@ -0,0 +1,30 @@
+// Readable streams / Writable Streams
+
+// Em Node, toda porta de entrada e saída é convertida em streams 
+
+// Streams -> 
+
+// process.stdin
+//   .pipe(process.stdout)
+
+import {Readable} from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout)
