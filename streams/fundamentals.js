1865dc324d296db1c6927a4eb481c704f5c45766
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-18 14:51:14 -0300

    Stream de escrita e transformação

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -7,7 +7,7 @@
 // process.stdin
 //   .pipe(process.stdout)
 
-import {Readable} from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -27,4 +27,21 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = -1 * Number(chunk.toString())
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
