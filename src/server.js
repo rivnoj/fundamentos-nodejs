08d1a776af95a7539a3cf86ce6ec21e6f086118c
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-18 16:40:50 -0300

    Criando middleware de JSON

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@
 //const http = require('http')
 // "node:" para diferenciar entre módulo interno e de terceiros
 import http from 'node:http' 
+import { json } from './middlewares/json.js'
 
 // GET, POST, PUT, PATCH, DELETE
 
@@ -26,22 +27,11 @@ const users = []
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     //early return
     return res
-            .setHeader('Content-type', 'application/json')
             .end(JSON.stringify(users))
   }
 
