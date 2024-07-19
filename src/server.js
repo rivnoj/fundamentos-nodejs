494e8669b700e109e0a52657a0f5814c9721f29d
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-19 12:22:34 -0300

    Route e Query parameters

--- src/server.js
+++ src/server.js
@@ -23,6 +23,25 @@ import { routes } from './routes.js'
 
 // HTTP Status Code
 
+/**
+ * Query parameters: exemplo, http://localhost:3333/users?userId=1 . 
+ * Usados quando a URL é stateful, para enviar informações não sensíveis 
+ * e não obrigatórios, que modificam a resposta, para filtragem.
+ */
+
+/**
+ * Route parameters: exemplo, http://localhost:3333/users/1 .
+ * Parâmetros não nomeados que aparecem na rota, no exemplo, o usuário com id = 1. 
+ * Usados para identificação de recursos. O método HTTP indica a operação que 
+ * deve ocorrer para o parâmetro. Não deve ser usado para enviar informações
+ * sensíveis.
+ */
+
+/**
+ * Request body: exemplo, http://localhost:3333/users . Envio  de informações de um 
+ * formulário, em geral, via HTTPS.
+ */
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
@@ -35,7 +54,7 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     return route.handler(req, res)
   }
-  
+
   return res.writeHead(404).end()
 })
 
