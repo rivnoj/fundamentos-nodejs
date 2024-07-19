3c48e8c482872d7ed899e2305f07dff8541a28a8
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-19 10:51:58 -0300

    Separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -7,8 +7,7 @@
 // "node:" para diferenciar entre módulo interno e de terceiros
 import http from 'node:http' 
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
-import { randomUUID } from 'node:crypto'
+import { routes } from './routes.js'
 
 // GET, POST, PUT, PATCH, DELETE
 
@@ -24,34 +23,19 @@ import { randomUUID } from 'node:crypto'
 
 // HTTP Status Code
 
-const database = new Database()
-
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-    
-    //early return
-    return res.end(JSON.stringify(users))
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
+
+  if (route) {
+    return route.handler(req, res)
   }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
-  }
-
+  
   return res.writeHead(404).end()
 })
 
